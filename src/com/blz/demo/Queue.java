18de850abc5ec187ad946b3dc2ccab794d0fbc0d
karthikbenki <karthikmc007@gmail.com>
2022-02-25 19:14:36 +0530

    Added: Queue Enque

---
+++ src/com/blz/demo/Queue.java
@@ -0,0 +1,20 @@
+package com.blz.demo;
+
+public class Queue {
+    LinkedList linkedList = new LinkedList();
+
+    public void enque(Object data){
+        linkedList.addLast(data);
+        displayQueue();
+    }
+
+    public void deque(){
+        linkedList.deleteFirst();
+        displayQueue();
+    }
+
+    public void displayQueue(){
+        linkedList.display();
+    }
+
+}
